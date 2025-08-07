c1541cd90ad4de3dada55b5bf737d4963369c8cb
Md Belayet Hossain <mdbelayethossain015@gmail.com>
2025-08-08 01:29:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // some data of users
 const users = [
     {id: 1, name: "Sabana", email: "sabana@gmail.com"},
-    {id: 1, name: "Shabnoor", email: "shabnoor@gmail.com"},
-    {id: 1, name: "Sabila", email: "sabila@gmail.com"},
+    {id: 2, name: "Shabnoor", email: "shabnoor@gmail.com"},
+    {id: 3, name: "Sabila", email: "sabila@gmail.com"},
 ]
+
+app.use(cors())
 
 // API
 app.get("/", (req, res) =>{
